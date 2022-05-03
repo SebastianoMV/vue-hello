aa80f5db4008067ab2f676c70f71dcb3900bdf5c
SebastianoMV <sebastianovisco@gmail.com>
2022-05-03 10:13:25 +0200

    Cambio immagine

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ const app = new Vue({
 
   data: {
     messaggio:'Ciaone',
-    immagine:'https://pbs.twimg.com/media/DIYJPGiUMAIKnTu.jpg'
-  }
+    immagine:'https://pbs.twimg.com/media/DIYJPGiUMAIKnTu.jpg',
+    flag: true,
+  },
   
+  methods: {
+    cambioImmagine(){
+      if(this.flag){
+       this.immagine = 'https://img-9gag-fun.9cache.com/photo/an44oR5_460s.jpg';
+      }else
+      {
+        this.immagine = 'https://pbs.twimg.com/media/DIYJPGiUMAIKnTu.jpg';
+       }
+       this.flag = !this.flag;
+    }
+  },
   
 });
